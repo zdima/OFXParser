c65f8b7941b517f978bc4cb8f5b35f3293c9a419
Dmitriy Zakharkin <mail@zdima.net>
2014-05-09 08:43:07 -0400

    Initial Commit

---
+++ OFXParser/OFXParser.m
@@ -0,0 +1,13 @@
+//
+//  OFXParser.m
+//  OFXParser
+//
+//  Created by Dmitriy Zakharkin on 5/9/14.
+//  Copyright (c) 2014 Dmitriy Zakharkin. All rights reserved.
+//
+
+#import "OFXParser.h"
+
+@implementation OFXParser
+
+@end
